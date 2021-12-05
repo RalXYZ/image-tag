0c8ab86fc0784d33d6b227f3e6bd8c48fb695f62
RalXYZ <RalXYZ@pm.me>
2021-12-05 22:05:42 +0800

    refactor: put sidebar into a component

---
+++ view/src/components/sidebar.tsx
@@ -0,0 +1,73 @@
+import * as React from "react";
+import { Toolbar } from "@mui/material";
+import MuiDrawer from "@mui/material/Drawer";
+import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
+import Divider from "@mui/material/Divider";
+import List from "@mui/material/List";
+import IconButton from "@mui/material/IconButton";
+import { styled } from "@mui/material/styles";
+import { mainListItems, secondaryListItems } from "../components/listItems";
+import { PinDropSharp } from "@mui/icons-material";
+
+const drawerWidth: number = 240;
+
+const Drawer = styled(MuiDrawer, {
+  shouldForwardProp: (prop) => prop !== "open",
+})(({ theme, open }) => ({
+  "& .MuiDrawer-paper": {
+    position: "relative",
+    whiteSpace: "nowrap",
+    width: drawerWidth,
+    transition: theme.transitions.create("width", {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    boxSizing: "border-box",
+    ...(!open && {
+      overflowX: "hidden",
+      transition: theme.transitions.create("width", {
+        easing: theme.transitions.easing.sharp,
+        duration: theme.transitions.duration.leavingScreen,
+      }),
+      width: theme.spacing(7),
+      [theme.breakpoints.up("sm")]: {
+        width: theme.spacing(9),
+      },
+    }),
+  },
+}));
+
+const Sidebar: React.FC<{
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}> = (props: {
+  open: boolean;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}) => {
+  return (
+    <Drawer variant="permanent" open={props.open}>
+      <Toolbar
+        sx={{
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "flex-end",
+          px: [1],
+        }}
+      >
+        <IconButton
+          onClick={() => {
+            props.setOpen(!open);
+          }}
+        >
+          <ChevronLeftIcon />
+        </IconButton>
+      </Toolbar>
+      <Divider />
+      <List>{mainListItems}</List>
+      <Divider />
+      <List>{secondaryListItems}</List>
+    </Drawer>
+  );
+};
+
+export default Sidebar;
